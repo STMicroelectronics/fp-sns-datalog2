34fd600d1bf1d7d8c5b2951dee4f2ec6cd6369f9
Simone Pradolini <simone.pradolini@st.com>
2024-04-11 11:48:20 +0200

    Added newly PnPLCompManager_conf (with "conf" lowercase). Aligned to v2.1.1 available on st.com

---
+++ Projects/STM32U585AI-SensorTile.boxPro/Applications/DATALOG2/Core/Inc/PnPLCompManager_conf.h
@@ -0,0 +1,50 @@
+/**
+  ******************************************************************************
+  * @file    PnPLCompManager_Conf.h
+  * @author  SRA
+  * @brief   PnPL Components Manager configuration template file.
+  ******************************************************************************
+  * @attention
+  *
+  * Copyright (c) 2022 STMicroelectronics.
+  * All rights reserved.
+  *
+  * This software is licensed under terms that can be found in the LICENSE file
+  * in the root directory of this software component.
+  * If no LICENSE file comes with this software, it is provided AS-IS.
+  *
+  *
+  ******************************************************************************
+  */
+
+/**
+  ******************************************************************************
+  * This file has been auto generated from the following Device Template Model:
+  * dtmi:vespucci:steval_mkboxpro:FP_SNS_DATALOG2_Datalog2;4
+  *
+  * Created by: DTDL2PnPL_cGen version 1.2.3
+  *
+  * WARNING! All changes made to this file will be lost if this is regenerated
+  ******************************************************************************
+  */
+
+/* Define to prevent recursive inclusion -------------------------------------*/
+#ifndef __PNPL_COMP_MANAGER_CONF_H__
+#define __PNPL_COMP_MANAGER_CONF_H__
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include "stm32u5xx_hal.h"
+#include <stdlib.h>
+
+/****************** PnPL Responses ***********************/
+#define PNPL_RESPONSES
+
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* __PNPL_COMP_MANAGER_CONF_H__*/
